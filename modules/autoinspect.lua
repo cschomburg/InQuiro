e1d577f3aaca766317df3b9a84afed900c5573b0
Constantin <xconstruct@gmail.com>
2009-08-24 22:23:50 +0200

    autoinspect fixes

--- modules/autoinspect.lua
+++ modules/autoinspect.lua
@@ -1,4 +1,6 @@
 local IQ = InQuiro
 IQ:RegisterModule("AutoInspect"):RegisterEvent("PLAYER_TARGET_CHANGED", function()
-	IQ:Inspect("target")
+	if(IQ.unit == "target" and UnitExists("target")) then
+		IQ:Inspect("target")
+	end
 end)
